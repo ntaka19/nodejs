dc24e9c8740a3eb6d44edb73bee6f0e586c3cebd
ntaka19 <naruakitakano@gmail.com>
2016-08-26 09:37:01 +0900

    added markov.js

---
+++ mecabNode/tasizan2-test.js
@@ -0,0 +1,5 @@
+var tasizan2 = require('./tasizan2.js').tasizan2;
+var tasizan3 = require('./tasizan2.js').tasizan3;
+
+console.log(tasizan2(3,5));
+console.log(tasizan3(2,3,4));
---
+++ mecabNode/tasizan2.js
@@ -0,0 +1,7 @@
+module.exports.tasizan2 = function (a,b) {
+	return a + b;
+};
+
+module.exports.tasizan3 = function (a,b,c){
+	return a + b + c;
+};
